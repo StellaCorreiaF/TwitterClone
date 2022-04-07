76cec810eef2886938c3f3cb6b291e54c4860f85
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-07 14:53:12 -0300

    adiciona classe main

---
+++ TwitterUser/src/main/java/io/ciaa/twitterUser/TwitterUserApplication.java
@@ -0,0 +1,11 @@
+package io.ciaa.twitterUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterUserApplication.class, args);
+    }
+}
