ab5feaf6d54198301c0994e759b1e2996fb36971
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-07 14:55:37 -0300

    cria método para ediçao de perfil

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-e
-5TwitterUser/src/main/resources/application.properties,6/c/6c022ea7a60a532a34a11fe2d4f8e5d3964f6b07
-H
-TwitterUser/build.gradle,0/5/05ae8dc87af0b91618f46cea557b09fb70adde93
+B
+docker-compose.yml,3/5/35b8c13cf2eb2a194eada000eb310d65aed53b2a
---
+++ TwitterUser/src/main/java/io/ciaa/twitterUser/service/ProfileService.java
@@ -0,0 +1,36 @@
+package io.ciaa.twitterUser.service;
+
+import io.ciaa.twitterUser.controller.ProfileMessage;
+import io.ciaa.twitterUser.modelo.Profile;
+import io.ciaa.twitterUser.repository.AccountRespository;
+import io.ciaa.twitterUser.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+    private final AccountRespository accountRespository;
+
+
+    @Autowired
+
+    public ProfileService(ProfileRepository profileRepository, AccountRespository accountRespository) {
+        this.profileRepository = profileRepository;
+        this.accountRespository = accountRespository;
+    }
+
+    public ProfileMessage updateProfile(Long id, ProfileMessage profileMessage){
+        if(profileRepository.findById(id).isPresent()){
+            Profile existingProfile = profileRepository.findById(id).get();
+            existingProfile.setName(profileMessage.getName());
+            existingProfile.setBirthDate(profileMessage.getBirthDate());
+            existingProfile.setDescription(profileMessage.getDescription());
+
+            Profile updatedProfile = profileRepository.save(existingProfile);
+            return new ProfileMessage(updatedProfile.getId(), updatedProfile.getName(), updatedProfile.getDescription(), updatedProfile.getBirthDate());
+        }
+        else {return null; }
+    }
+}
