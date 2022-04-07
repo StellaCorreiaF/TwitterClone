e15e9c2b6b170fad5aaaa79c3579d4c86bca5829
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-07 16:22:56 -0300

    adiciona metodos para listar e editar accounts

---
+++ TwitterUser/src/main/java/io/ciaa/twitterUser/controller/AccountController.java
@@ -0,0 +1,37 @@
+package io.ciaa.twitterUser.controller;
+
+import io.ciaa.twitterUser.modelo.Account;
+import io.ciaa.twitterUser.repository.AccountRespository;
+import io.ciaa.twitterUser.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Component
+@RequestMapping("/user/account")
+public class AccountController {
+
+    private final AccountRespository accountRespository;
+    private final AccountService accountService;
+
+    public AccountController(AccountRespository accountRespository, AccountService accountService) {
+        this.accountRespository = accountRespository;
+        this.accountService = accountService;
+    }
+
+    @Autowired
+
+
+    @GetMapping()
+    public List<Account> getAccounts() {
+        return this.accountService.getAccount();
+    }
+
+    @PutMapping("/{id}")
+    public AccountMessage updateAccunt(@PathVariable Long id, AccountMessage accountMessage){
+        return accountService.updateAccount(id, accountMessage);
+    }
+}
