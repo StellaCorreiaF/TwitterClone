5d593f197526717f9e1f455d76b79442510fe740
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-07 16:30:44 -0300

    cria módulo para tweet

---
+++ TwitterTweet/src/main/java/io/ciaa/twittertweet/TwitterTweetApplication.java
@@ -0,0 +1,13 @@
+package io.ciaa.twittertweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterTweetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterTweetApplication.class, args);
+    }
+
+}
