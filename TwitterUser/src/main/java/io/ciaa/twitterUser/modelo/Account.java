2bec8027a9546f467c7e953e6dd7dc3c86541fc8
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-07 16:19:58 -0300

    otimiza imports

--- TwitterUser/src/main/java/io/ciaa/twitterUser/modelo/Account.java
+++ TwitterUser/src/main/java/io/ciaa/twitterUser/modelo/Account.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
-import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
