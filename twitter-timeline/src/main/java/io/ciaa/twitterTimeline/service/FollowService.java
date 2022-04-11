c0bd6d1635cd7d16233e322da51fb31cad45b995
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-11 17:44:25 -0300

    cria metodo para seguir usuario utilizando o user client

--- twitter-timeline/src/main/java/io/ciaa/twitterTimeline/service/FollowService.java
+++ twitter-timeline/src/main/java/io/ciaa/twitterTimeline/service/FollowService.java
@@ -1,35 +1,40 @@
 package io.ciaa.twitterTimeline.service;
 
-import io.ciaa.twitterTimeline.FollowDto;
+import io.ciaa.twitterTimeline.adapter.UserClient;
+
+import io.ciaa.twitterTimeline.controller.FollowDto;
 import io.ciaa.twitterTimeline.model.Follow;
+import io.ciaa.twitterTimeline.model.FollowFactory;
 import io.ciaa.twitterTimeline.repository.FollowRepository;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowService {
 
+    private final FollowRepository followRepository;
+    private final UserClient userClient;
+
     @Autowired
-    FollowRepository followRepository;
-
-
-    public FollowService(FollowRepository followRepository) {
+    public FollowService(FollowRepository followRepository, UserClient userClient) {
         this.followRepository = followRepository;
-
+        this.userClient = userClient;
     }
 
     @SneakyThrows
-   public void followUser(Long followerId, Long followedId) {
+    public Follow followUser(FollowDto followDto) {
 
-        Follow follower = new Follow();
-        follower.setFollower(followerId);
-        follower.setFollowed(followedId);
-        followRepository.save(follower);
+       var follower = userClient.get(followDto.getUserFollowerId());
+       var followed = userClient.get(followDto.getUserFollowedId());
 
 
+        var newFollow = new FollowFactory().create(follower.getUserName(), followed.getUserName());
+        followRepository.save(newFollow);
+
+        return newFollow;
     }
+
     @SneakyThrows
     public void unfollowUser(Long followerId, Long followedId)
     {
