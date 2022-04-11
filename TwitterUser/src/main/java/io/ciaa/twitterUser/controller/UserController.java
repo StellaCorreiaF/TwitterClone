d4fd9bdecba2cd484a47ea55f4c933f879a8c7aa
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-11 17:50:37 -0300

    cria metodo para listar usuarios

--- TwitterUser/src/main/java/io/ciaa/twitterUser/controller/UserController.java
+++ TwitterUser/src/main/java/io/ciaa/twitterUser/controller/UserController.java
@@ -1,28 +1,39 @@
 package io.ciaa.twitterUser.controller;
 
 import io.ciaa.twitterUser.modelo.Account;
+import io.ciaa.twitterUser.repository.AccountRespository;
 import io.ciaa.twitterUser.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Component
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final AccountService accountService;
+    private final AccountRespository accountRespository;
 
     @Autowired
-    public UserController(AccountService accountService) {
+    public UserController(AccountService accountService, AccountRespository accountRespository) {
         this.accountService = accountService;
+        this.accountRespository = accountRespository;
     }
 
     @PostMapping
     public Account createUser(@RequestBody AccountMessage message){
         return accountService.createUser(message);
     }
+
+    @GetMapping("/{userid}")
+    public Account getUser(@PathVariable("userid") Long userid) {
+        return accountRespository.getById(userid);
+    }
+    // criar um endpoint para retornar os usuarios @GettMapping("/id")
+
+    @GetMapping()
+    public List<Account> getUsers() {return accountRespository.findAll(); }
 }
