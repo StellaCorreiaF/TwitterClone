04fd5ecca0b3d5bf38abdaea936cb6b79fae50b7
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-11 17:45:17 -0300

    interliga serviço de timeline ao user

--- twitter-timeline/src/main/java/io/ciaa/twitterTimeline/controller/TimelineController.java
+++ twitter-timeline/src/main/java/io/ciaa/twitterTimeline/controller/TimelineController.java
@@ -1,22 +1,27 @@
 package io.ciaa.twitterTimeline.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.ciaa.twitterTimeline.adapter.UserClient;
 import io.ciaa.twitterTimeline.adapter.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Component
 @RequestMapping("/timeline")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
 public class TimelineController {
     @Autowired
     UserClient userClient;
 
-    @GetMapping()
-
+    @GetMapping("/users/{id}")
     //posso excluir o parametro e inserir diretamente na requisiçao
-    public UserDto createUser(Long id) {
+    public UserDto createUser(@PathVariable Long id) {
         return userClient.get(id);
     }
 }
