f8d061ce543a9f3e656981447e9911ade72de719
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-11 17:48:49 -0300

    cria metodo para criar tweet

---
+++ TwitterTweet/src/main/java/io/ciaa/twittertweet/service/TweetService.java
@@ -0,0 +1,35 @@
+package io.ciaa.twittertweet.service;
+
+import io.ciaa.twittertweet.adpter.UserClient;
+import io.ciaa.twittertweet.controller.TweetMessage;
+import io.ciaa.twittertweet.model.Tweet;
+import io.ciaa.twittertweet.model.TweetFactory;
+import io.ciaa.twittertweet.repository.TweetRepository;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class TweetService {
+    private final TweetRepository tweetRepository;
+    private final UserClient userClient;
+
+    @Autowired
+    public TweetService(TweetRepository tweetRepository, UserClient userClient) {
+        this.tweetRepository = tweetRepository;
+        this.userClient = userClient;
+    }
+
+
+    @SneakyThrows
+    public Tweet createTweet(Long id, TweetMessage tweetMessage){
+        var account = userClient.get(id);
+        var newTweet = new TweetFactory().create(tweetMessage.getTweet(), account.getUserName());
+        var createdTweet = tweetRepository.save(newTweet);
+
+        return createdTweet;
+    }
+
+
+}
