235a35486cf3e5ab56c391c1a8344f2ff7b05699
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-04-12 17:10:43 -0300

    cria metodo para listar tweets por usuário no repositório

--- TwitterTweet/src/main/java/io/ciaa/twittertweet/repository/TweetRepository.java
+++ TwitterTweet/src/main/java/io/ciaa/twittertweet/repository/TweetRepository.java
@@ -3,5 +3,8 @@ package io.ciaa.twittertweet.repository;
 import io.ciaa.twittertweet.model.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    List<Tweet> getByUserName(String userName);
 }
